3b0445582e7cdf1c1d56297553269ad73ecefdf8
chuang] <33791604+chuang861012@users.noreply.github.com>
2018-09-07 01:57:56 +0800

    del utils

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export default function (query = null, page = 1) {
-    const url = query ? `http://127.0.0.1:5000/get?query=${query}&page=${page}` : `http://127.0.0.1:5000/get?page=${page}`;
-    return fetch(url).then(res => res.json())
-        .then((res) => {
-            return res
-        });
-}
